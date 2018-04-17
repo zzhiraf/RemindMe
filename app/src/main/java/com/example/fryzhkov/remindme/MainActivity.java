84a09280756451039db9f97ea0289cd8ad488dcb
zzhiraf <zzhiraf@yandex.ru>
2018-04-17 08:54:12 +0300

    Добавили тулбар и кнопку поиска

--- app/src/main/java/com/example/fryzhkov/remindme/MainActivity.java
+++ app/src/main/java/com/example/fryzhkov/remindme/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.fryzhkov.remindme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
